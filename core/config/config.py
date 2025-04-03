755fb7d961e3c55962c3fba2b39a0e6d767e1945
Kelven <kelvenr40@gmail.com>
2025-04-03 12:59:52 -0300

    REFACTOR, update in cors

--- core/config/config.py
+++ core/config/config.py
@@ -23,18 +23,21 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api-auten_auth/login")
 
 
 def config_CORS(app):
-    # paths onde o front ira enviar dados para o backend
+    from fastapi.middleware.cors import CORSMiddleware
+
     origins = [
         "http://localhost.tiangolo.com",
         "https://localhost.tiangolo.com",
-        "http://localhost",
+        "http://localhost:5173/", # react
         "http://localhost:8080",
     ]
 
     app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+    expose_headers=["X-Custom-Header"],
+    max_age=3600,
     )
